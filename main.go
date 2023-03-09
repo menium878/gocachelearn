e2b0755c7450a12c054e7c827f551d5934810904
unknown <meus13@gmail.com>
2023-03-09 23:20:55 +0100

    Komentarze + dzialanie wyrzucania automatycznego kluczy

--- main.go
+++ main.go
@@ -7,30 +7,35 @@ import (
 )
 
 func main() {
-	gc := gcache.New(5). // size odpowiada za ilość Setów które mogę mieć na daną chwilę gdy jedne mamy error przy wywołaniu już 2
-				LRU(). // how it pop out the cache przyda się jak będę ją zwalniał
+	gc := gcache.New(5). // size odpowiada za ilość Setów które mogę mieć na daną chwilę gdy jedne mamy error przy wywołaniu już 2 działa na kluczach tylko wartością zmieniam wartość niezależnie ilość się nie zmienia cacha
+				LRU(). // how it pop out the cache przyda się jak będę ją zwalniał zwalniamy najmniej używaną tutaj w tym wywołujemy get więc key jest używane więcej razy
 				Build()
 	gc.Set("key", "ok")
 	gc.Set("key1", "no")
-	gc.Set("key", "ok")
-	gc.Set("key", "ok")
-	gc.Set("key", "ok")
-	gc.Set("key", "ok")
-	gc.Set("key", "hallo")
+	gc.Set("key2", "ok")
+	gc.Set("key3", "ok")
 	value, err := gc.Get("key")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Get:", value)
+	gc.Set("key4", "ok")
+	gc.Set("key5", "ok")
+	gc.Set("key6", "hallo")
+
+	fmt.Println(gc.Keys(true))
+
 	value1, err := gc.Get("key1")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Get:", value1)
-	gc.Remove("key") // usuwanie manualnej
-	value, err = gc.Get("key")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Get:", value)
+	/*
+		gc.Remove("key") // usuwanie manualnej
+		value, err = gc.Get("key")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Get:", value)
+	*/
 }
